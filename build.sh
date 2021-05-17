c4e99d6b07561b11e06f59b5db4b359f40621b2a
WingLim <643089849@qq.com>
2021-05-17 22:43:58 +0800

    refactor: split out build function

--- build.sh
+++ build.sh
@@ -2,23 +2,25 @@
 
 export CADDY_VERSION=v2.4.0
 
+function build() {
+    export $1
+    export $2
+    xcaddy build \
+        --with github.com/WingLim/caddy-webhook \
+        --output $3
+}
+
 linuxArch=("amd64" "386" "arm64" "ppc64le")
 for arch in "${linuxArch[@]}"; do
-    GOOS=linux GOARCH=${arch} xcaddy build \
-        --with github.com/WingLim/caddy-webhook \
-        --output bin/caddy-"${arch}"
+    build GOOS=linux GOARCH=${arch} bin/caddy-"${arch}"
 done
 
 armVersion=("6" "7")
 for ver in "${armVersion[@]}"; do
-    GOOS=linux GOARCH=arm GOARM=${ver} xcaddy build \
-        --with github.com/WingLim/caddy-webhook \
-        --output bin/caddy-armv"${ver}"
+    build GOARCH=arm GOARM=${ver} bin/caddy-armv"${ver}"
 done
 
 unsupportUPX=("s390x")
 for arch in "${unsupportUPX[@]}"; do
-    GOOS=linux GOARCH=${arch} xcaddy build \
-        --with github.com/WingLim/caddy-webhook \
-        --output ubin/caddy-"${arch}"
+    build GOOS=linux GOARCH=${arch} ubin/caddy-"${arch}"
 done
