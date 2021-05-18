33b3ac246f037825218bec4407ac9d5c96be6352
WingLim <643089849@qq.com>
2021-05-18 16:47:28 +0800

    chore: split out release tagged

--- build.sh
+++ build.sh
@@ -2,26 +2,33 @@
 
 export CADDY_VERSION=v2.4.0
 
+tag=$1
+if [[ $tag == "" ]]; then
+tag="latest"
+fi
+
+echo $tag
+
 function build() {
-    export $1
     export $2
+    export $3
     xcaddy build \
-        --with github.com/WingLim/caddy-webhook \
+        --with github.com/WingLim/caddy-webhook@$1 \
         --with github.com/abiosoft/caddy-exec \
-        --output $3
+        --output $4
 }
 
 linuxArch=("amd64" "386" "arm64" "ppc64le")
 for arch in "${linuxArch[@]}"; do
-    build GOOS=linux GOARCH=${arch} bin/caddy-"${arch}"
+    build $tag GOOS=linux GOARCH=${arch} bin/caddy-"${arch}"
 done
 
 armVersion=("6" "7")
 for ver in "${armVersion[@]}"; do
-    build GOARCH=arm GOARM=${ver} bin/caddy-armv"${ver}"
+    build $tag GOARCH=arm GOARM=${ver} bin/caddy-armv"${ver}"
 done
 
 unsupportUPX=("s390x")
 for arch in "${unsupportUPX[@]}"; do
-    build GOOS=linux GOARCH=${arch} ubin/caddy-"${arch}"
+    build $tag GOOS=linux GOARCH=${arch} ubin/caddy-"${arch}"
 done
